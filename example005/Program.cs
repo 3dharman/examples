ee16eff8816e131d8267a5e299be2ecf77c03849
dharman <featlord@yandex.ru>
2023-02-09 21:52:26 +0300

    added favoriteuserConsole

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Варвара")
+{
+    Console.WriteLine("Hello, my princess, have a nice day!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
