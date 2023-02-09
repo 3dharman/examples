8718fd8433fb497608dbe564d85fc298c789b33b
dharman <featlord@yandex.ru>
2023-02-09 21:20:29 +0300

    added sumConsole

---
+++ example003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Начало
+// Определить первое число
+// Определить второе число
+// Вывести сумму введеных чисел
+// Конец
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
