54120101a84608783c6e674bd815ac90527fd5da
dharman <featlord@yandex.ru>
2023-02-10 12:20:44 +0300

    added findConsole

---
+++ example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = { 14 , 2 , 31 , 4, 58 , 15 , 16 , 27 , 58 };
+
+int n = array.Length;
+int find = 58;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
